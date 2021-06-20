adf4316469fd5ccc7fa1689bf37333efc626e985
JCrosby0 <jcrosby@internode.on.net>
2021-06-20 16:07:42 +0800

    frontend cms config

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,13 +3,24 @@
     <div class="text-lg">
       Willetton Primary School Parents and Citizens Association
     </div>
-    <div>> Menu</div>
+    <div>{{ categories }}</div>
+    <ul>
+      <li v-for="(cat, i) in categories" :key="'cat'+i">
+        {{ cat }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
 // TODO: get menu content from netlify CMS
+  async asyncData ({ $content }) {
+    const categories = await $content('categories').fetch()
+    return {
+      categories
+    }
+  }
 }
 </script>
 
