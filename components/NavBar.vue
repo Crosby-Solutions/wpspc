5441a207839e0a230738f1315df70e4abb5c791c
JCrosby0 <jcrosby@internode.on.net>
2021-06-20 14:14:06 +0800

    initial commit

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row justify-between w-full bg-yellow-600 p-4">
+    <div>WPS P&C</div>
+    <div>> Menu</div>
+  </div>
+</template>
+
+<script>
+export default {
+// TODO: get menu content from netlify CMS
+}
+</script>
+
+<style>
+
+</style>
